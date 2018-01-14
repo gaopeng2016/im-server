592648394786bb4684f9755d943ff98bb265a93c
gaopeng <453482397@qq.com>
2018-01-14 18:24:44 +0800

    正常操作

---
+++ server/constants/ErrorCode.js
@@ -0,0 +1,5 @@
+class ErrorCode {
+    static USER_NAME_IS_EXISED = 1000;
+}
+
+module.exports = ErrorCode;
--- server/routers/api.js
+++ server/routers/api.js
@@ -3,9 +3,12 @@
  */
 
 const router = require('koa-router')();
-const userInfoController = require('./../controllers/user-info')
+const UserController = require('./../controllers/UserController')
 
-const api = router.get('/user/getUserInfo.json', userInfoController.getLoginUserInfo);
+const api = router.get('/user/:userName', UserController.getUserInfoByUserName)
+    .get('/chat', async (ctx) => {
+        await ctx.render('index');
+    });
     // .post('/user/signIn.json', userInfoController.signIn)
     // .post('/user/signUp.json', userInfoController.signUp)
 
